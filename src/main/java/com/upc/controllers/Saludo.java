f771c069613a11cc24d3387093424d6762c2e857
sebastian123gonzalo <100171673+sebastian123gonzalo@users.noreply.github.com>
2025-06-24 18:12:08 -0500

    feat: change logic

---
+++ src/main/java/com/upc/controllers/Saludo.java
@@ -0,0 +1,15 @@
+package com.upc.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/saludo")
+public class Saludo {
+    @GetMapping
+    public String saludar() {
+        return "¡Hola desde Jenkins y Spring Boot!";
+    }
+}
